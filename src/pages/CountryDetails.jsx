d4c96d7695641d198d0e5b911cbb6c18e4e1daf3
Sulabh Shrestha <shresthasulabh777@gmail.com>
2023-08-24 00:22:02 +0545

    Country data are displayed in CountryDetails page

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -20,11 +20,35 @@ function CountryDetails() {
 
   console.log(queryCountryDetails);
   return (
-    <>
+    <div className="h-[100vh] px-[4%]">
       <Link to={"/"}>
-        <button className="bg-green-400 px-3 py-2 rounded-lg">Back</button>
+        <button className="dark:bg-very-dark-blue bg-very-light-gray px-3 py-2 rounded-lg shadow-md dark:shadow-dark-gray">
+          Back
+        </button>
       </Link>
-    </>
+
+      <div className="country-details flex flex-col sm:flex-row gap-12 items-center">
+        <img
+          src={queryCountryDetails.flags.png}
+          alt={queryCountryDetails.flag.alt}
+          className="shadow-md w-full h-[10%] sm:flex-1"
+        />
+
+        <div className="details p-4 flex-1 text-left">
+          <h1 className="font-bold text-2xl py-4">
+            {queryCountryDetails.name.common}
+          </h1>
+          <p>Population: {queryCountryDetails.population}</p>
+          <p>Region: {queryCountryDetails.continents[0]}</p>
+          <p>
+            Capital:{" "}
+            {typeof queryCountryDetails.capital == "undefined"
+              ? ""
+              : queryCountryDetails.capital.join(", ")}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
